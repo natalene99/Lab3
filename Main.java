970f3e478e88a7342db16186b25133a988de0236
natalene <natalene@mail.uc.edu>
2021-02-02 17:51:56 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Enter a number to count down or up from");
+    Scanner s = new Scanner(System.in);
+    
+    int number = s.nextInt();
+    if (number >= 0)
+    {
+      while (number >= 0)
+      {
+        System.out.println(number);
+        number --;
+      }
+    }
+
+    else if (number <0)
+    {
+      while (number <0)
+      {
+        System.out.println(number);
+        number ++;
+      }
+    }
+  
+  System.out.println("Blast off!");
+
+  }
+}
